7ea663ae32ebb70cb22d161f468443a62bf7ca58
Jacob <jhp4986@gmail.com>
2023-08-21 15:58:29 +0900

    modify: Router.js file

--- src/Router.js
+++ src/Router.js
@@ -1,18 +1,18 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
-import BulletinBoard from './BulletinBoard/BulletinBoard';
-import Login from './Login/Login';
+import BulletinBoard from './Pages/BulletinBoard/BulletinBoard'; // 수정된 경로
+import Login from './Pages/Login/Login'; // 수정된 경로
 
-const  Router = () => {
+const Router = () => {
   return (
     <BrowserRouter>
         <Routes>
-            <Route path="/" element={<Login/>} />
-            <Route path="/bulletinboard" element={<BulletinBoard/>} />
+            <Route path="/" element={<Login />} />
+            <Route path="/bulletinboard" element={<BulletinBoard />} />
         </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default Router
+export default Router;
