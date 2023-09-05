727d9e5cdc383a30514a4e64612f13d8feed6cc1
Jacob <jhp4986@gmail.com>
2023-09-05 14:13:22 +0900

    modify: PostDetail.js

--- src/Pages/BulletinBoard/PostDetail.js
+++ src/Pages/BulletinBoard/PostDetail.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import './PostDetail.css'
 
 const PostDetail = () => {
 const { postId } = useParams();
 const [postContent, setPostContent] = useState({}); // 포스트 데이터를 저장할 상태 변수
+// const [editing, setEditing]
 
 useEffect(() => {
     async function fetchPostData() {
@@ -13,7 +15,7 @@ useEffect(() => {
         throw new Error("포스트를 불러올 수 없습니다.");
         }
         const postData = await response.json();
-        const selectedPost = postData.mockData.find((post) => post.id.toString() === postId); // postId를 사용하여 포스트 선택
+        const selectedPost = postData.mockData.find((post) => post.id.toString() === postId);
         if (!selectedPost) {
         throw new Error("포스트를 찾을 수 없습니다.");
         }
@@ -28,10 +30,12 @@ useEffect(() => {
 
 return (
     <div>
-    <h2>포스트 상세 정보</h2>
-    <h3>{postContent.title}</h3>
-    <p>{postContent.content}</p>
-    <div>{postContent.date}</div>
+        <div className="postDetailTitle">게시글 상세 정보</div>
+        <div className="postDetailContent">
+            <div className="postTitle">{postContent.title}</div>
+            <div className="postContent">{postContent.content}</div>
+            <div className="postDate">{postContent.date}</div>
+        </div>
     </div>
 );
 };
